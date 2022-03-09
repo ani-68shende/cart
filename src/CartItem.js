6f7c36e5d3610d8496ded6e0fe516d41554d3c9e
ani-68 <ani68shende@github.com>
2022-03-09 10:39:05 +0530

    added the cart.js and made few changes in index.css file

--- src/CartItem.js
+++ src/CartItem.js
@@ -10,6 +10,25 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQuantity = () => {
+        this.setState({
+            qty: this.state.qty + 1
+        })
+        //if we have repeative setState queries, we just execute the last one
+    }
+    decreaseQuantity = () => {
+        const { qty } = this.state;
+        if (qty === 0) {
+            return;
+        }
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty - 1
+            }
+        }, () => {
+            console.log(this.state.qty);
+        });
+    }
     render() {
         //object destructuring to get the values from the state object
         const { price, title, qty } = this.state;
@@ -24,9 +43,9 @@ class CartItem extends React.Component {
                     <div className="cart-item-actions">
                         {/* Buttons*/}
                         <img className="action-icons" alt="increase"
-                            action-icons src="https://cdn-icons-png.flaticon.com/512/992/992651.png" />
+                            action-icons src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQuantity} />
                         <img className="action-icons" alt="decrease"
-                            action-icons src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+                            action-icons src="https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick={this.decreaseQuantity} />
                         <img className="action-icons" alt="delete"
                             action-icons src="https://cdn-icons.flaticon.com/png/512/484/premium/484611.png?token=exp=1646401450~hmac=497ca88647bb78aeb7b1227c0935ea58" />
                     </div>
